3b9f732e5f6c4757086ab17f3e9aa123b4886fe5
Elijah Mylcev <elijahmylcev@gmail.com>
2022-03-17 12:46:22 +0300

    Remaining Components

--- crud/src/components/app/app.js
+++ crud/src/components/app/app.js
@@ -1,13 +1,16 @@
 import './app.css';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 function App() {
   return (
     <div className="app">
       <AppInfo />
       <SearchPanel />
-      <div className="container" />
+      <EmployeesList />
+      <EmployeesAddForm />
     </div>
   );
 }
