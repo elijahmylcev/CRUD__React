dde44a62323dfdb0d96f0cab218c47bed78e7af9
Elijah Mylcev <elijahmylcev@gmail.com>
2022-03-22 18:33:48 +0300

    added sass in project

--- crud/src/components/employees-add-form/employees-add-form.js
+++ crud/src/components/employees-add-form/employees-add-form.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import './employees-add-form.css';
+// import './employees-add-form.css';
+import './employees-add-form.scss';
 
 class EmployeesAddForm extends Component {
   constructor(props) {
